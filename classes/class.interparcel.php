821f837511421d28418a3b20b3f7e4b39e91ce5e
SithVicious <paul@nwit.info>
2021-05-30 02:18:56 +1000

    Update class.interparcel.php

--- classes/class.interparcel.php
+++ classes/class.interparcel.php
@@ -63,7 +63,7 @@
 				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 				CURLOPT_CUSTOMREQUEST => 'GET',
 				CURLOPT_HTTPHEADER => array(
-					'X-Interparcel-Auth: ' . $this->apiKey, #TODO: Move X-Interparcel-Auth to config.php
+					'X-Interparcel-Auth: ' . $this->apiKey,
 					'Content-Type: application/json'
 				),
 			));
